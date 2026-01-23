92e292da53f957358b94aad80c3e28b54417f178
Roushan kumar <roushankumar7231246@gmail.com>
2026-01-23 13:40:11 +0530

    get-nth-fin

---
+++ src/recursion/GetNthFib.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class GetNthFib {
+
+    public static int getNthFib(int n) {
+        if(n <= 1){
+            return 0;
+        }
+
+        if(n==2){
+            return 1;
+        }
+        return getNthFib(n-1) + getNthFib(n-2);
+    }
+}
