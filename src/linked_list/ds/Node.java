079c0772d145dc03454f9d1c878fd3fade4fe82e
Roushan kumar <roushankumar7231246@gmail.com>
2026-03-20 00:33:45 +0530

    doubly linked list

---
+++ src/linked_list/ds/Node.java
@@ -0,0 +1,11 @@
+package linked_list.ds;
+
+public class Node {
+    public int value;
+    public Node prev;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
