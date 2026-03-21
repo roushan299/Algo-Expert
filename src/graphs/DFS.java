6085a33a638644b621bdd205a0bbcd2015215576
Roushan kumar <roushankumar7231246@gmail.com>
2026-03-21 20:15:17 +0530

    depth first search

---
+++ src/graphs/DFS.java
@@ -0,0 +1,33 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFS {
+
+
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(this.name);
+
+            for(Node child : this.children) {
+                child.depthFirstSearch(array);
+            }
+
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
