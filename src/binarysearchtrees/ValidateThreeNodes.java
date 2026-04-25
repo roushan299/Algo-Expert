66e8f1416b4d060d14f8f2ebf10b3692d7b9e4d8
Roushan kumar <roushankumar7231246@gmail.com>
2026-04-26 00:03:50 +0530

    validate three node

---
+++ src/binarysearchtrees/ValidateThreeNodes.java
@@ -0,0 +1,13 @@
+package binarysearchtrees;
+
+import binarysearchtrees.ds.BST;
+
+public class ValidateThreeNodes {
+
+    public boolean validateThreeNodes(BST nodeOne, BST nodeTwo, BST nodeThree) {
+        // Write your code here.
+        return false;
+    }
+
+
+}
